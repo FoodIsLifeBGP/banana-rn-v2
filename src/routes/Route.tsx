487effb2b23bbf1a52779203521962908e629cc0
Ren Burnett <ren.burnett@mobyinc.com>
2023-07-21 18:52:17 -0700

    glaring errors seem to be taken care of 🤞

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,13 +1,9 @@
 import React from "react";
+import getEnv from "@util/environment";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-
-// TODO: remove this disabled lint eventually
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// import * as colors from "@util/constants/colors";
-import getEnv from "@util/environment";
 
 import MakeClaimScreen from "../screens/MakeClaimScreen/MakeClaimScreen";
 import LoginScreen from "../screens/LoginScreen";
@@ -25,7 +21,6 @@ import DonorDonationScreen from "../screens/DonorDashboardScreen/DonorDonationSc
 import DonorHistoryScreen from "../screens/DonorHistoryScreen/DonorHistoryScreen";
 import MapScreen from "../screens/MapScreen/MapScreen";
 
-import MenuDrawer from "../elements/MenuDrawer/MenuDrawer";
 import MainOption from "../elements/MenuDrawer/MainOption/MainOption";
 import SubOption from "../elements/MenuDrawer/SubOption/SubOption";
 import ClaimDetailsScreen from "../screens/ClaimDetailsScreen/ClaimDetailsScreen";
@@ -38,7 +33,7 @@ const Stack = createNativeStackNavigator();
 function StackNavigator() {
   return (
     <Stack.Navigator
-      // headerShown="false" //TODO: why isnt this prop allowed
+      screenOptions={{ headerShown:false }}
       initialRouteName="LoginSuccessScreen"
     >
       <Stack.Screen
@@ -101,38 +96,38 @@ function donorOrClientDrawer() {
 
   const DONOR_MENU = {
     QRCodeScannerScreen: {
-      screen: StackNavigator,
-      navigationOptions: { drawerLabel: <MainOption text="Scan QR Code" icon="qrCode" /> },
+      component: StackNavigator,
+      options: { drawerLabel: () => <MainOption text="Scan QR Code" icon="qrCode" /> },
     },
     DonorDashboardScreen: {
-      screen: StackNavigator,
-      navigationOptions: { drawerLabel: <MainOption text="Donations" icon="claims" /> },
+      component: StackNavigator,
+      options: { drawerLabel: () => <MainOption text="Donations" icon="claims" /> },
     },
     DonorHistoryScreen: {
-      screen: StackNavigator,
-      navigationOptions: { drawerLabel: <SubOption text="History" /> },
+      component: StackNavigator,
+      options: { drawerLabel: () => <SubOption text="History" /> },
     },
   };
 
   const CLIENT_MENU = {
     DashboardScreen: {
-      screen: StackNavigator,
-      navigationOptions: { drawerLabel: <MainOption text="Donations" icon="donations" /> },
+      component: StackNavigator,
+      options: { drawerLabel: () => <MainOption text="Donations" icon="donations" /> },
     },
     ClientClaimsScreen: {
-      screen: StackNavigator,
-      navigationOptions: { drawerLabel: <MainOption text="Claims" icon="claims" /> },
+      component: StackNavigator,
+      options: { drawerLabel: () => <MainOption text="Claims" icon="claims" /> },
     },
     ClientHistoryScreen: {
-      screen: StackNavigator,
-      navigationOptions: { drawerLabel: <SubOption text="History" /> },
+      component: StackNavigator,
+      options: { drawerLabel: () => <SubOption text="History" /> },
     },
   };
 
   const COMMON_MENU = {
     ContactScreen: {
-      screen: (props) => <ContactScreen {...props} />,
-      navigationOptions: { drawerLabel: <MainOption text="Contact Us" icon="help" /> },
+      component: (props) => <ContactScreen {...props} />,
+      options: { drawerLabel: () => <MainOption text="Contact Us" icon="help" /> },
     },
   };
 
@@ -147,34 +142,41 @@ function donorOrClientDrawer() {
     };
 }
 
-// Drawer Navigator
+/* Drawer Navigator */
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
-  Object.entries(donorOrClientDrawer()).forEach((ting) =>
-    console.log("ting", ting));
-  // IMPORTANT TODO: figure out why Drawer.Screen is erroring and remove the above console.log
+  const menuOptions = donorOrClientDrawer();
+
   return (
     <Drawer.Navigator>
-      {/* {Object.entries(donorOrClientDrawer()).map(([ name, config ]) => (
+      {/*
+         Create a Screen for each side drawer menu option.
+         The name and component are taken from the key-value pairs in menuOptions.
+         The options object is passed as is.
+      */}
+      {Object.entries(menuOptions).map(([name, config]) => (
         <Drawer.Screen
           key={name}
           name={name}
-          {...config}
+          component={config.component}
+          options={config.options}
         />
-      ))} */}
+      ))}
     </Drawer.Navigator>
   );
 }
 
-// Full App Navigation - Includes Non-Logged in Screens
+/* Full App Navigation - Includes Non-Logged in Screens */
 const FullAppStack = createNativeStackNavigator();
 
 function FullAppStackNavigator() {
   return (
     <FullAppStack.Navigator
-      // headerShown="false" TODO: why aren't these two props working?
-      // defaultNavigationOptions={{ gestureEnabled: false }}
+      screenOptions={{
+        headerShown:false,
+        gestureEnabled: false,
+      }}
       initialRouteName="LoginScreen"
     >
       <FullAppStack.Screen
